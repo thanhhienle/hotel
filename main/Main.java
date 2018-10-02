cb1bd26f4d7e85030c0d083ea3cc4415128a1c6b
thanhhienle <38900321+thanhhienle@users.noreply.github.com>
2018-10-03 00:02:39 +0700

    Add files via upload

---
+++ main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+import gui.FLogin;
+/**
+ *
+ * @author ICHIGEKI
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FLogin f = new FLogin();
+        f.setVisible(true);
+    }
+}
